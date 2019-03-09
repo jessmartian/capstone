7f4d3b4cc8b3bf3238b256ce8773a8cd1bd46eb4
Derek Martin <Entertech\dmartin@a-1v40gkwogf113.corp.entertechsystems.com>
2019-03-09 13:06:35 -0500

    intialize project, create sidebar

---
+++ elements/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <div :class="[introScreen ? 'main-container--intro': '', 'main-container']">
+        <Intro></Intro>
+      </div>
+    </div>
+    <div class="sidebar-container">
+      <Sidebar v-on:item-clicked="sidebarNavigation"></Sidebar>
+    </div>
+  </div>
+</template>
+
+<script>
+import Intro from './components/Intro.vue';
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Intro,
+    Sidebar,
+  },
+  data() {
+    return {
+      introScreen: true,
+      fincherScreen: false,
+      wrightScreen: false,
+      tarantinoScreen: false,
+    }
+  },
+  methods: {
+    sidebarNavigation(sidebarItem) {
+      console.log(sidebarItem)
+      switch(sidebarItem) {
+        case 'Intro':
+          this.introScreen = !this.introScreen
+          this.fincherScreen = false
+          this.wrightScreen = false
+          this.tarantinoScreen = false
+          break
+        case 'Fincher':
+          this.fincherScreen = !this.fincherScreen
+          this.introScreen = false
+          this.wrightScreen = false
+          this.tarantinoScreen = false
+          break
+        case 'Wright':
+          this.wrightScreen = !this.wrightScreen
+          this.introScreen = false
+          this.fincherScreen = false
+          this.tarantinoScreen = false
+          break
+        case 'Tarantino':
+          this.tarantinoScreen = !this.tarantinoScreen
+          this.introScreen = false
+          this.fincherScreen = false
+          this.wrightScreen = false
+          break
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #FFFFFF;
+  background-color: #2B2B31;
+  height: 100vh;
+  display: flex;
+}
+.main {
+  width: 90%;
+  margin: 3em 0 3em 3em;
+  &-container {
+    border: 2px solid #FFFFFF;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &--intro {
+      border: none
+    }
+  }
+}
+.sidebar-container {
+  width: 10%;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ elements/src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-item" 
+      v-for="item in items" 
+      :key="item"
+      @click="itemClicked(item)">
+        {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data() {
+    return {
+      items: [
+        'Intro',
+        'Fincher',
+        'Wright',
+        'Tarantino'
+      ]
+    }
+  },
+  methods: {
+    itemClicked(item) {
+      this.$emit('item-clicked', item)
+    },
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .sidebar {
+    writing-mode: vertical-rl;
+    text-orientation: mixed;
+    display: flex;
+    justify-content: center; 
+  }
+  .sidebar {
+    display: flex; 
+    justify-content: space-evenly;
+    &-item {
+      cursor: pointer;
+      padding: 2em 1em;
+      border:  1px solid #2B2B31;
+      &:hover {
+        border:  1px solid #FFFFFF
+      }
+    }
+  }
+</style>
