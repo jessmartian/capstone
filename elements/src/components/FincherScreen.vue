fe316f72ee32709eb96ac4495745544e7780a416
jessmartian <jessmartian97@gmail.com>
2019-03-20 23:32:44 -0400

    colour changes

--- elements/src/components/FincherScreen.vue
+++ elements/src/components/FincherScreen.vue
@@ -6,13 +6,13 @@
     <div class="fincher__container">
       <div class="fincher__description">
         <div class="fincher__description-title">Zodiac</div>
-        <div class="fincher__description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a fermentum magna. Suspendisse vitae ligula vitae diam imperdiet finibus. Integer interdum, dolor non pellentesque elementum, nulla mi commodo mauris, id tincidunt lectus dolor sit amet turpis. Donec diam mi, aliquam id vestibulum id, posuere eu tortor.</div>
+        <div class="fincher__description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a fermentum magna. Suspendisse vitae ligula vitae diam imperdiet finibus. Integer interdum, dolor non pellentesque elementum, nulla mi commodo mauris, id tincidunt lectus dolor sit amet turpis. Donec diam mi, aliquam id v estibulum id, posuere eu tortor.</div>
       </div>
       <div class="fincher__selected">
         <div class="fincher__selected-screen">
           <video controls
             class="fincher__selected-screen--video">
-            <source :src="video" type="video/mp4">
+            <source src="../assets/videos/Fincher/FClubOne.mp4" type="video/mp4">
           </video>
         </div>
         <div class="fincher__selected-palette">
@@ -26,11 +26,11 @@
     </div>
     <div class="fincher__palettes">
       <div class="fincher__palettes-palette"
-        v-for="palette in palettes" 
+        v-for="palette in palettes"
         :key="palette.video"
         @click="selectPalette(palette)">
         <div class="fincher__palettes-palette-color"
-          v-for="color in palette.colors" 
+          v-for="color in palette.colors"
           :key="color"
           :style="{ 'background-color': color }">
         </div>
@@ -44,51 +44,71 @@ export default {
   name: 'FincherScreen',
   computed: {
     video() {
-      return this.selectedPallete.video
-    }
+      return this.selectedPallete.video;
+    },
   },
   data() {
     return {
       selectedPallete: {
-        video: '../assets/videos/Fincher/FightClub-Endingscene.mp4',
+        video: '../assets/videos/Fincher/FClubOne.mp4',
         colors: [
-          '#291F0F',
-          '#0E0800',
-          '#D7D2C5',
-          '#537C72',
-          '#AA7D47',
-        ]
+          '#677645',
+          '#D2DDAB',
+          '#8C8B7E',
+          '#A3A176',
+          '#58543A',
+        ],
       },
-      //TO DO: change all the colours plz
+      //TO DO: change all the colours plz//
       palettes: [
         {
-          video: '../assets/videos/Fincher/FightClubInsomniascene.mp4',
+         video: '../assets/videos/Fincher/FClubTwo.mp4',
           colors: [
-            '#291F0F',
-            '#0k0800',
-            '#D7l2C5',
-            '#538C72',
-            '#AA7D47',
-          ]
+            '#70714C',
+            '#A4A06F',
+            '#950000',
+            '#EA0000',
+            '#280000',
+          ],
         },
         {
           video: 'source of video here1',
           colors: [
-            '#292F0F',
-            '#0E0900',
-            '#D7F2C5',
-            '#537D72',
-            '#AA7E47',
-          ]
+            '#0D313F',
+            '#004E5E',
+            '#8A5D28',
+            '#A9805A',
+            '#0C0B0E',
+          ],
         },
-      ]
-     }
+        {
+          video: 'source of video here1',
+          colors: [
+            '#2B365D',
+            '#B9B906',
+            '#A49E74',
+            '#C18D00',
+            '#5D2F01',
+          ],
+        },
+        {
+          video: 'source of video here1',
+          colors: [
+            '#677645',
+            '#D2DDAB',
+            '#8C8B7E',
+            '#A3A176',
+            '#58543A',
+          ],
+        },
+      ],
+    };
   },
   methods: {
     selectPalette(palette) {
-      this.selectedPallete = palette
-      console.log(palette)
-    }
+      this.selectedPallete = palette;
+      console.log(palette);
+    },
   },
 };
 </script>
@@ -107,7 +127,7 @@ export default {
     font-size: 2em;
   }
   &__container {
-    display:flex; 
+    display:flex;
     justify-content: space-between;
     padding: 2em 10em;
     height: 40vh;
@@ -122,12 +142,12 @@ export default {
     &-body {
       font-size: 16px;
       font-family: 'Open Sans', sans-serif;
-      width: 60%;
+      width: 90%;
       line-height: 2em;
     }
   }
   &__selected {
-    width: 655px;
+    width: 640px;
     display: flex;
     flex-direction: column;
     &-screen {
@@ -152,16 +172,16 @@ export default {
   &__palettes {
     display: flex;
     justify-content: space-around;
-    align-items: center; 
+    align-items: center;
     margin: 4em auto;
-    background:#FFFFFF;
     width: 80%;
-    height: 15%;
+    height: 10%;
     &-palette {
       display: flex;
       height: 50%;
       width: 25%;
-      background-color: black;
+      background-color: white;
+      padding: 1%;
       cursor: pointer;
       &-color {
         width: 20%;
